641193ea3659f08d51634b040f160a5bfa9fa6bb
Maxim <121682068+m-melikhov@users.noreply.github.com>
2024-10-19 21:50:26 +0300

    Виправлення стилів і посилань на соц.мережі

--- src/assets/components/Footer.tsx
+++ src/assets/components/Footer.tsx
@@ -6,25 +6,25 @@ const Footer: React.FC = () => {
     <footer className="footer">
       <div className="social-icons">
         <a
-          href="https://www.instagram.com"
+          href="https://www.instagram.com/annastanish_/"
           target="_blank"
           rel="noreferrer"
         >
           Instagram
         </a>
         <a
-          href="https://www.facebook.com"
+          href="https://www.facebook.com/profile.php?id=100009306202353"
           target="_blank"
           rel="noreferrer"
         >
           Facebook
         </a>
         <a
-          href="https://www.twitter.com"
+          href="https://t.me/annastanish"
           target="_blank"
           rel="noreferrer"
         >
-          Twitter
+          Telegram
         </a>
       </div>
       <p>&copy; 2024 My Photography Portfolio. All rights reserved.</p>
