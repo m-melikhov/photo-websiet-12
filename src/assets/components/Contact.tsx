b973fb2ae234b85f7c79bb97acf4c3a9552cb98d
Maxim <121682068+m-melikhov@users.noreply.github.com>
2024-10-19 20:40:33 +0300

    зміна в формі

--- src/assets/components/Contact.tsx
+++ src/assets/components/Contact.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-// import axios from 'axios';
 import '../styles/Contact.css';
 
 interface ContactForm {
@@ -30,7 +29,7 @@ const Contact: React.FC = () => {
 
     try {
       const response = await fetch(
-        'https://photo-websiet.vercel.app/api/send-message',
+        'https://photo-websiet.vercel.app/api/send-message', // Оновлений URL для API
         {
           method: 'POST',
           headers: {
