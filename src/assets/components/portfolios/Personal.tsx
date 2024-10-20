57f0a0cdbc9ef077ba00da1850746056302356a8
Maxim <121682068+m-melikhov@users.noreply.github.com>
2024-10-21 00:01:18 +0300

    Зміни в посиланнях

--- src/assets/components/portfolios/Personal.tsx
+++ src/assets/components/portfolios/Personal.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Personal: React.FC = () => {
   return (
     <div>
-      <h2>Особисті Проекти</h2>
+      <h2>Особисті Проекти!</h2>
       {/* Додайте свій контент тут */}
     </div>
   );
