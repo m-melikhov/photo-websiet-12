cbaea94f71a4453da38383b996220f2208e296a8
Maxim <121682068+m-melikhov@users.noreply.github.com>
2024-10-21 16:33:48 +0300

    Додавання файлів і стилів в файл Персонал

--- src/assets/components/portfolios/Personal.tsx
+++ src/assets/components/portfolios/Personal.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import personal1 from '../../styles/images/personal1.jpg';
+import personal2 from '../../styles/images/personal2.jpg';
+import personal3 from '../../styles/images/personal3.jpg';
+import personal4 from '../../styles/images/personal1.jpg';
+import personal5 from '../../styles/images/personal2.jpg';
+import personal6 from '../../styles/images/personal3.jpg';
+import '../../styles/portfolios/Personal.css';
 
 const Personal: React.FC = () => {
   return (
     <div>
       <h2>Особисті Проекти!</h2>
-      {/* Додайте свій контент тут */}
+      <div className="personal-gallery-grid">
+        <img
+          src={personal1}
+          alt="Personal 1"
+          className="grid-item grid-item-1"
+        />
+        <img
+          src={personal2}
+          alt="Personal 2"
+          className="grid-item grid-item-2"
+        />
+        <img
+          src={personal3}
+          alt="Personal 3"
+          className="grid-item grid-item-3"
+        />
+        <img
+          src={personal4}
+          alt="Personal 4"
+          className="grid-item grid-item-4"
+        />
+        <img
+          src={personal5}
+          alt="Personal 5"
+          className="grid-item grid-item-5"
+        />
+        <img
+          src={personal6}
+          alt="Personal 6"
+          className="grid-item grid-item-6"
+        />
+      </div>
     </div>
   );
 };
